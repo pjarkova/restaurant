966f1e89cc0cadedc85f3e9f7c57a220bef4f0f6
pjarkova <160063451+pjarkova@users.noreply.github.com>
2024-05-28 17:21:21 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Browser</title>
+</head>
+
+<body>
+    <header>
+        <h2>Main</h2>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#about">About us</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="home">
+      <h2>Travelers' Choice</h2>
+      <p>Welcome to our restaurant website! <br> Indulge your senses in a culinary journey like no other in the heart of Riga. Our restaurant offers an exquisite dining experience</p>
+      <div class="restaurant">
+      <img src="restaurant.jpeg" alt="Our restaurant" width="450" height="300">
+    </section>
+  
+  	<section id="menu">
+      <h2>Menu</h2>
+      <table>
+        <thead>
+        	<tr>
+          		<th>Meal</th>
+          		<th>Price</th>
+        	</tr>
+        </thead>
+        <tbody>
+        	<tr>
+          		<td>Meal1</td>
+              	<td>$10.00</td>
+        	</tr>
+          	<tr>
+          		<td>Meal2</td>
+              	<td>$12.00</td>
+        	</tr>
+            <tr>
+          		<td>Meal3</td>
+              	<td>$8.00</td>
+        	</tr>
+        </tbody>
+      </table>
+  	</section>
+  
+  	<section id="about">
+      <h2>About us</h2>
+      <h3>Meet Our Team</h3>
+      <p>Our team is the heart and soul of what we do. Each member brings a unique blend of talent, passion, and dedication, creating a dining experience that is truly exceptional.</p>
+     <div class="team">
+       <img src="team.jpeg" alt="Restaurant Team" width="450" height="300">
+    </section>
+    
+    <section id="Contact">
+      
+      <h2>Contact information</h2>
+      <h3>Address</h3>
+      	<ul>
+          <li>Street</li>
+      	</ul>
+      <h3>Call us</h3>
+      	<ul>
+          <li>(+371)12345678</li>
+      	</ul>
+      <h3>Email</h3>
+      	<ul>
+          <li><a href="#email">restaurant@restaurant.com</a></li>
+      	</ul>
+       
+     <footer>
+       <p>&copy; Company name.<br> All Rights Reserved.</p>
+     </footer>
+    <script src="script.js"></script>
+</body>
+
+</html>
+  <script src="script.js"></script>
+</body>
+
+</html>
+  <script src="script.js"></script>
+</body>
+
+</html>
